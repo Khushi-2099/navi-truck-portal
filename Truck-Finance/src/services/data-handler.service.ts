646e7801d3715fdc028adc274ee1ff7f257cb60f
ASL\prasadd.patil <prasadd.patil_con@aurionpro.com>
2023-11-08 15:19:14 +0530

    Prasad's Commit: Updated specifications of Truck cards and updated JSON data in service

--- Truck-Finance/src/services/data-handler.service.ts
+++ Truck-Finance/src/services/data-handler.service.ts
@@ -24,59 +24,70 @@ export class DataHandlerService {
     this.vehicleList = list;
   } 
 
+  formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0
+  })
+
+  getVehicleListMaster(): any[] {
+    return this.vehicleDetails;
+  }
+
   vehicleDetails = [
     {
       name: '2024 LoneStar',
       imgUrl: '../assets/NaviStar/2024-Lonestar/2024-loanstar-homepage-card.png',
-      price: 180822,
+      price: this.formatter.format(180822),
       engineType:	'CUMMINS X15',
       maxPower:	'MAX HP 605' ,
-      maxTorque:	'2,050 LB-FT. TORQUE',
+      maxTorque:	'2,050lb',
       GVWR:	'60.6K',
+      style: [{'border-radius': `50%`}]
     },
     {
       name: '2024 RH613',
       imgUrl: '../assets/NaviStar/2024-RH613/2024-RH613-homepage-card .png',
-      price: 180822,
-      engineType:	'International A26',
+      price: this.formatter.format(180822),
+      engineType:	'Int. A26',
       maxPower:	'MAX HP 515' ,
-      maxTorque:	'1,850 LB-FT. TORQUE',
+      maxTorque:	'1,850lb',
       GVWR:	'60K',
     },
     {
       name: '2024 CV515',
       imgUrl: '../assets/NaviStar/2023-CV515/2023-CV515-homepage-card.png',
-      price: 180822,
-      engineType:	'International 6.6',
+      price: this.formatter.format(180822),
+      engineType:	'Intl. 6.6',
       maxPower:	'MAX HP 350' ,
-      maxTorque:	'700 LB-FT. TORQUE',
+      maxTorque:	'700lb',
       GVWR:	'19.5K',
     },
     {
       name: '2024 MV607',
       imgUrl: '../assets/NaviStar/2024-MV607/2024-MV607-homepage-card.png',
-      price: 180822,
+      price: this.formatter.format(180822),
       engineType:	'CUMMINS L9',
       maxPower:	'MAX HP 360' ,
-      maxTorque:	'1,150 LB-FT. TORQUE',
+      maxTorque:	'1,150lb',
       GVWR:	'54.6K',
     },
     {
       name: '2024 HV513',
       imgUrl: '../assets/NaviStar/2024-HV513/2024-HV513-homepage-card.png',
-      price: 180822,
-      engineType:	'International A26',
+      price: this.formatter.format(180822),
+      engineType:	'Intl. A26',
       maxPower:	'MAX HP 515' ,
-      maxTorque:	'1,850 LB-FT. TORQUE',
+      maxTorque:	'1,850lb',
       GVWR:	'60K',
     },
     {
       name: '2024 HX620',
       imgUrl: '../assets/NaviStar/2024-HX620/2024-HX620-homepage-card.png',
-      price: 180822,
+      price: this.formatter.format(180822),
       engineType:	'CUMMINS X15',
       maxPower:	'MAX HP 605' ,
-      maxTorque:	'2,050 LB-FT. TORQUE',
+      maxTorque:	'2,050lb',
       GVWR:	'74K',
     }
   ]
